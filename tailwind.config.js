9d91110d1b596ccf2c74dacf35ffaae1ad263775
JuanMosquella <94761627+JuanMosquela@users.noreply.github.com>
2023-09-13 17:25:38 -0300

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors:{
+      colors: {
         "dark-green": "#5ECC76",
-        "white": "#FFF",
-        "black": "#0E0E0E",
-        "green": "#68FF89"
-
+        white: "#FFF",
+        black: "#0E0E0E",
+        green: "#68FF89",
       },
       screens: {
-        'tall': { 'raw': '(min-height: 70vh)' },
+        tall: { raw: "(min-height: 70vh)" },
       },
       container: {
-        center:true,        
+        center: true,
         padding: {
-          DEFAULT: "1rem",
+          DEFAULT: "0rem",
           md: "2rem",
-          xl: "0rem"
-        }
-      }
+          xl: "0rem",
+        },
+      },
     },
   },
   plugins: [],
-}
+};
